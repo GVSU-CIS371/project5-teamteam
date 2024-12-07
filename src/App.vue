651dcd9f53d99fc3bb44cfa79198de20f8da9a15
Alec Boerman <boermaal@mail.gvsu.edu>
2024-12-07 16:45:53 -0500

    Added add feature to app.vue

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,14 @@
           <v-icon>{{ link.icon }}</v-icon>
           {{ link.text }}
         </v-btn>
+
+      <!--button added for dialog-->
+        <v-btn class="mx-5" @click="addProductDialog = true" >
+          <v-icon>mdi-plus</v-icon>
+          Add Product
+        </v-btn>
+      <!--button added for dialog-->
+
       </v-app-bar>
       <v-main class="bg-blue-lighten-5">
         <router-view v-slot="{ Component }">
@@ -13,19 +21,77 @@
             <component :is="Component" />
           </transition>
         </router-view>
+
       </v-main>
-  
+
       <v-footer color="primary" app>
         © 2023 My Online Store. All rights reserved.
       </v-footer>
+
+      <!--Start of additions for dialog used to add products-->
+      <v-dialog v-model="addProductDialog" max-width="600">
+        <v-card>
+          <v-card-title>Add New Product</v-card-title>
+          <v-card-text>
+            <v-form ref="form">
+              <v-text-field
+                label="Product Name"
+                v-model="newProduct.name"
+                required
+              ></v-text-field>
+              <v-text-field
+                label="Image URL"
+                v-model="newProduct.imgURL"
+                required
+              ></v-text-field>
+              <v-text-field
+                label="Description"
+                v-model="newProduct.description"
+                required
+              ></v-text-field>
+              <v-text-field
+                label="Category"
+                v-model="newProduct.category"
+                required
+              ></v-text-field>
+              <v-slider
+                v-model="newProduct.rating"
+                label="Rating"
+                min="1"
+                max="5"
+                step="1"
+                thumb-label="always"
+              ></v-slider>
+              <v-text-field
+                label="Price"
+                v-model="newProduct.price"
+                type="number"
+                required
+              ></v-text-field>
+              <v-text-field
+                label="Stock"
+                v-model="newProduct.stock"
+                type="number"
+                required
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn text="Close" @click="addProductDialog = false">Close</v-btn>
+            <v-btn color="primary" text="Add" @click="saveProduct">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!--End of additions for dialog used to add products-->
+
     </v-app>
   </template>
-  
+
   <script lang="ts" setup>
   import { ref } from "vue";
-  // Importing product Pinia store we created. 
+  // Importing product Pinia store we created.
   import { useProductStore } from "./stores/ProductStore";
-  // Assigning product store to our variable 
+  // Assigning product store to our variable
   const productStore = useProductStore();
   // Calling the function to populating the store with items.
   productStore.init()
@@ -37,5 +103,40 @@
     { text: "Groceries", to: "/groceries", icon: "mdi-cart" },
     { text: "Best Seller", to: "/bestseller", icon: "mdi-cash-register" },
   ]);
+
+// start of additions for add dialog feature
+  const addProductDialog = ref(false);
+  const newProduct = ref({
+    name: "",
+    imgURL: "",
+    description: "",
+    category: "",
+    rating: 3,
+    price: 0,
+    stock: 0,
+  });
+
+  const saveProduct = () => {
+    if (newProduct.value.name && newProduct.value.description && newProduct.value.price > 0) {
+      productStore.products.push({
+        id: Date.now().toString(),
+        data: {
+          name: newProduct.value.name,
+          description: newProduct.value.description,
+          image: newProduct.value.imgURL,
+          rating: newProduct.value.rating,
+          price: newProduct.value.price,
+          stock: newProduct.value.stock,
+          category: newProduct.value.category,
+        },
+      });
+      console.log(newProduct.value);
+      addProductDialog.value = false;
+
+      newProduct.value = { name: "", description: "", rating: 3, price: 0, imgURL: "", stock: 0, category: "" };
+    } else {
+      console.log("info missing");
+    }
+  };
+// end of additions for add dialog feature
   </script>
-  
