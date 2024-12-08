ea21953ec2328700480ebfe3f03c0663d3a0b337
Alec Boerman <boermaal@mail.gvsu.edu>
2024-12-08 16:42:03 -0500

    both modify and delete are working with firebase but page doesn't update

--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -22,16 +22,79 @@
                     <v-icon class="mr-2">mdi-delete</v-icon>
                     Delete
                 </v-btn>
+              <v-btn class="mx-5" @click="modifyProductDialog = true" >
+                <v-icon>mdi-plus</v-icon>
+                Modify
+              </v-btn>
             </v-card-actions>
 
         </v-card>
     </v-hover>
+
+  <!--Start of additions for dialog used to modify products-->
+      <v-dialog v-model="modifyProductDialog" max-width="600">
+        <v-card>
+          <v-card-title>Add New Product</v-card-title>
+          <v-card-text>
+            <v-form ref="form">
+              <v-text-field
+                label="Product Name"
+                v-model="modifiedProduct.name"
+                required
+                disabled
+              ></v-text-field>
+              <v-text-field
+                label="Image URL"
+                v-model="modifiedProduct.imgURL"
+                required
+              ></v-text-field>
+              <v-text-field
+                label="Description"
+                v-model="modifiedProduct.description"
+                required
+              ></v-text-field>
+              <v-text-field
+                label="Category"
+                v-model="modifiedProduct.category"
+                required
+              ></v-text-field>
+              <v-slider
+                v-model="modifiedProduct.rating"
+                label="Rating"
+                min="1"
+                max="5"
+                step="1"
+                thumb-label="always"
+              ></v-slider>
+              <v-text-field
+                label="Price"
+                v-model="modifiedProduct.price"
+                type="number"
+                required
+              ></v-text-field>
+              <v-text-field
+                label="Stock"
+                v-model="modifiedProduct.stock"
+                type="number"
+                required
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn text="Close" @click="modifyProductDialog = false">Close</v-btn>
+            <v-btn color="primary" text="Add" @click="modifyProduct()">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!--End of additions for dialog used to modify products-->
 </template>
 
 <script lang="ts" setup>
 
+import {ref} from "vue";
 import { Product } from '../types/product';
 const product = defineProps<Product>();
+
 import {db} from "../firebase.ts";
 import {
   deleteDoc,
@@ -42,7 +105,8 @@ import {
   query,
   where,
   getDocs,
-  QuerySnapshot
+  QuerySnapshot,
+  updateDoc
 } from "firebase/firestore";
 
 function deleteProduct() {
@@ -63,5 +127,45 @@ function deleteProduct() {
       await (deleteDoc(myDoc));
     })
   });
+}
+//code for modify
+const modifyProductDialog = ref(false);
+const modifiedProduct = ref({
+  name: product.name,
+  imgURL: product.image,
+  description: product.description,
+  category: product.category,
+  rating: product.rating,
+  price: product.price,
+  stock: product.stock,
+});
+
+function modifyProduct(){
+    const confirmation = window.confirm(
+    `Are you sure you want to Modify the product: ${product.name}?`
+  );
+  if (!confirmation) {
+    console.log("Deletion canceled by the user.");
+    return;
+  }
+  const productName = modifiedProduct.value.name;
+  console.log(productName);
+  const myCol: CollectionReference = collection(db, "products");
+  const qr = query(myCol, where("name", "==", productName));
+  getDocs(qr).then((qs: QuerySnapshot) => {
+    qs.forEach(async (qd: QueryDocumentSnapshot) => {
+      const myDoc = doc(db, "products", qd.id);
+      await updateDoc(myDoc, {
+        name: modifiedProduct.value.name,
+        description: modifiedProduct.value.description,
+        price: modifiedProduct.value.price,
+        rating: modifiedProduct.value.rating,
+        stock: modifiedProduct.value.stock,
+        image: modifiedProduct.value.imgURL,
+        category: modifiedProduct.value.category,
+      });
+    })
+  });
+  modifyProductDialog.value = false;
 }
 </script>
