b6a09895988449f6205b8b34fd2842f9bc5c4597
Alec Boerman <boermaal@mail.gvsu.edu>
2024-12-08 14:52:38 -0500

    Removing from firebase works

--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -15,6 +15,10 @@
             </v-card-text>
             <v-img :src="product.image" alt="image of the item being displayed" height="228px" class="mt-10"></v-img>
             <v-card-text class="px-12 pt-10">{{ product.description }}</v-card-text>
+            <v-btn class="mx-5" @click="deleteProduct()" >
+              <v-icon>mdi-plus</v-icon>
+              Add Product
+            </v-btn>
 
         </v-card>
     </v-hover>
@@ -23,5 +27,31 @@
 <script lang="ts" setup>
 
 import { Product } from '../types/product';
-const product = defineProps<Product>()
+const product = defineProps<Product>();
+import {db} from "../firebase.ts";
+import {
+  deleteDoc,
+  doc,
+  collection,
+  CollectionReference,
+  QueryDocumentSnapshot,
+  query,
+  where,
+  getDocs,
+  QuerySnapshot
+} from "firebase/firestore";
+
+function deleteProduct() {
+  const productName = product.name;
+  console.log(productName);
+  const myCol: CollectionReference = collection(db, "products");
+  const qr = query(myCol, where("name", "==", productName));
+  getDocs(qr).then((qs: QuerySnapshot) => {
+    qs.forEach(async (qd: QueryDocumentSnapshot) => {
+      const myDoc = doc(db, "products", qd.id);
+      await (deleteDoc(myDoc));
+    })
+  });
+  location.reload();
+}
 </script>
